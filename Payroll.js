5cebcd6e2e56e53f5833756206f742a84b68ed79
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-05-02 16:23:34 +0530

    [sanjana] Add : On save create object and save on local obj

--- Payroll.js
+++ Payroll.js
@@ -1,11 +1,11 @@
 //UC1
 class EmployeeData{
 
-    get id(){ return this._id}
+    get id(){ return this._id;}
     set id(id){
             this._id=id;
     }
-    get name(){ return this._name}
+    get name(){ return this._name;}
     set name(name){
     let Name_Reg=RegExp('^[A-Z]{1}[a-z]{2,}');
             if(Name_Reg.test(name))
@@ -17,7 +17,7 @@ class EmployeeData{
                 throw 'Name is incorrect !';
             }
     }
-    get profilePic(){return this._profilePic }
+    get profilePic(){return this._profilePic; }
     set profilePic(profilePic){
         this._profilePic=profilePic;
     }
@@ -31,26 +31,22 @@ class EmployeeData{
             this._department=department;
         
     }
-   get salary(){return this._salary}
+   get salary(){return this._salary;}
     set salary(salary){
             this._salary = salary;
         
     }
    
-    get note(){ return this._note}
+    get note(){ return this._note;}
     set note(note){
             this._note=note;
        
     }
-    get startdate(){ return this._startdate}
-    set startdate(startdate){
-        let ms = Date.now();
-        if(startdate<=ms)
-        {
+    get startdate(){ return this._startdate;}
+    set startdate(startdate){{
             this._startdate=startdate;
-        }
-        else throw 'Date is Incorrect !';
-       
+    }
+      
     }
    
     tostring()
@@ -60,7 +56,6 @@ class EmployeeData{
                         this.startdate.toLocaleDateString("en-US",options);
         return "Id = "+this.id +" , Name = "+this.name+" , Gender = "+ this.gender+" , profilePic = "+this.profilePic+" , Department = "+ this.department+" , Salary = "+this.salary+" StartDate = "+empDate+" , note = "+ this.note;
     }
+
+    
 }
-
-
-
